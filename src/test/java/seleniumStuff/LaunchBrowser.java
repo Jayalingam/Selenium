ecb538643b451841b4c0c2f51180826548bb0e1b
Jayalingam <ajayalinga@gmail.com>
2025-11-03 14:37:08 +0530

    Browser

--- src/test/java/seleniumStuff/LaunchBrowser.java
+++ src/test/java/seleniumStuff/LaunchBrowser.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchBrowser {
 	
 	public static void main (String args[]) {
-		System.setProperty("webdriver.chrome.driver", "D:\\Users\\ajayalingam\\eclipse-workspace\\SeleniumLearning\\drivers\\chrome-win32\\chrome-win32\\chrome.exe");
+		System.setProperty("webdriver.chrome.driver", "F:\\Jaga\\Selenium\\drivers\\chromedriver-win32\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 
 	}
