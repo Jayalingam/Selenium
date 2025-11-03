f2a00589e0092aee87685da9c5f740f9dabac98e
jayalingama <jayalingama@verifone.com>
2025-11-03 17:13:39 +0530

    Lanch Browser

--- src/test/java/seleniumStuff/LaunchBrowser.java
+++ src/test/java/seleniumStuff/LaunchBrowser.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchBrowser {
 	
 	public static void main (String args[]) {
-		System.setProperty("webdriver.chrome.driver", "F:\\Jaga\\Selenium\\drivers\\chromedriver-win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver-win32\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 
 	}
